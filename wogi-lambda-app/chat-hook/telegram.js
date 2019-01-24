617e34e96dda27bc9fec0f7ec579fbcd4b413eec
Jun Qi <jqtan@thoughtworks.com>
2019-01-24 15:26:28 +0800

    Handle callback query in telegram webhook

--- wogi-lambda-app/chat-hook/telegram.js
+++ wogi-lambda-app/chat-hook/telegram.js
@@ -1,10 +1,12 @@
 const TelegramBot = require('node-telegram-bot-api');
-const User = require('./user').User;
+const { User } = require('./user');
+const { MessageDelivery } = require('./message-delivery.model');
 
-const TOKEN = '720915328:AAENtjqp36A3JFK4WEAuOFjmi8kFF-uPTEA';
-const bot = new TelegramBot(TOKEN);
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
 
-// event.body looks something like this:
+const isValidOtp = messageText => messageText.match(/^WOGI-REG.*/);
+
+// For a normal message, event.body looks something like this:
 // {
 //     "update_id": 540942475,
 //     "message": {
@@ -27,51 +29,141 @@ const bot = new TelegramBot(TOKEN);
 //     }
 // }
 
-exports.handler = async (event, context) => {
-  try {
-    const body = JSON.parse(event.body)
-    const messageText = body.message.text;
-    const chatId = body.message.chat.id;
+const handleMessage = async (message) => {
+  const messageText = message.text;
+  const chatId = message.chat.id;
 
-    if (!isValidOtp(messageText)) {
-      await bot.sendMessage(chatId, "Please enter a valid OTP to register.");
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          errors: [
-            {
-              message: "Invalid OTP given",
-            },
-          ],
-        }),
-      };
-    }
+  let user;
+  user = await User.queryOne('chatId').eq(chatId).exec();
 
-    const otp = messageText;
-    const user = await User.queryOne('otp').eq(otp).exec();
-
-    if (!user) {
-      await bot.sendMessage(chatId, "We couldn't find a user associated with the given OTP. Please try and generate a new one");
-
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          errors: [
-            {
-              message: 'Could not find a user associated with the given OTP.'
-            }
-          ]
-        })
-      }
-    }
-
-    user.platform = "TELEGRAM";
-    user.chatId = chatId;
-    await user.save()
-    await bot.sendMessage(chatId, "You have successfully registered with Wogi on Telegram!");
+  if (user) {
+    await bot.sendMessage(chatId, 'It looks like you\'ve already registered for Notifications.sg. You\'re already able to receive notifications for government agencies through Telegram.');
 
     return {
       statusCode: 204,
+    };
+  }
+
+  if (!isValidOtp(messageText)) {
+    await bot.sendMessage(chatId, 'Please enter a valid OTP to register.');
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        errors: [
+          {
+            message: 'Invalid OTP given',
+          },
+        ],
+      }),
+    };
+  }
+
+  const otp = messageText;
+  user = await User.queryOne('otp').eq(otp).exec();
+
+  if (!user) {
+    await bot.sendMessage(chatId, "We couldn't find a user associated with the given OTP. Please try and generate a new one");
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        errors: [
+          {
+            message: 'Could not find a user associated with the given OTP.',
+          },
+        ],
+      }),
+    };
+  }
+
+  user.platform = 'TELEGRAM';
+  user.chatId = chatId;
+  await user.save();
+  await bot.sendMessage(chatId, 'You have successfully registered with Wogi on Telegram!');
+
+  return {
+    statusCode: 204,
+  };
+};
+
+// For a callback query, event.body looks something like this:
+// {
+//   "update_id": 540942564,
+//   "callback_query": {
+//     "id": "1212848847398988314",
+//     "from": {
+//       "id": 282388377,
+//       "is_bot": false,
+//       "first_name": "Jun Qi (she/her)",
+//       "username": "tjjjwxzq",
+//       "language_code": "en"
+//     },
+//     "message": {
+//       "message_id": 233,
+//       "from": [
+//         null
+//       ],
+//       "chat": [
+//         null
+//       ],
+//       "date": 1548311835,
+//       "text": "Hello from WOGI"
+//     },
+//     "chat_instance": "5261849522625467999",
+//     "data": "7abcfe40-1fa2-11e9-a3dd-7fdc84f60b52"
+//   }
+// }
+
+const handleCallbackQuery = async (callbackQuery) => {
+  const { data, from, message } = callbackQuery;
+  const { optionSelected, messageDeliveryId } = JSON.parse(data);
+  const chatId = from.id;
+
+  const messageDelivery = await MessageDelivery.get(messageDeliveryId);
+
+  if (!messageDelivery) {
+    bot.sendMessage(chatId, `Something went wrong with handling the reply to the message: ${message.text}`);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        errors: [
+          {
+            message: `Message delivery not found for message delivery id: ${messageDeliveryId}`,
+          },
+        ],
+      }),
+    };
+  }
+
+  if (messageDelivery.responseStatus && messageDelivery.responseStatus !== 'PENDING') {
+    bot.sendMessage(chatId, 'You have already responded to this option, don\'t guai lan:)');
+    return {
+      statusCode: 204,
+    };
+  }
+  bot.sendMessage(chatId, 'Thank you! Your response has been sent to the agency');
+  messageDelivery.responseStatus = optionSelected;
+
+  await messageDelivery.save();
+
+  return {
+    statusCode: 204,
+  };
+};
+
+exports.handler = async (event) => {
+  try {
+    console.log('EVENT HANDLED BY TELEGRAM', event);
+    const body = JSON.parse(event.body);
+    console.log('EVENT BODY HANDLED BY TELEGRAM', body);
+
+    if (body.message) {
+      return await handleMessage(body.message);
+    }
+
+    if (body.callback_query) {
+      return await handleCallbackQuery(body.callback_query);
     }
   } catch (e) {
     return {
@@ -83,10 +175,6 @@ exports.handler = async (event, context) => {
           },
         ],
       }),
-    }
+    };
   }
-}
-
-const isValidOtp = (messageText) => {
-  return messageText.match(/^WOGI-REG.*/);
-}
+};
--- wogi-lambda-app/create-message-deliveries/message-delivery.model.js
+++ wogi-lambda-app/create-message-deliveries/message-delivery.model.js
@@ -1,32 +1,35 @@
-const dynamoose = require("dynamoose");
+const dynamoose = require('dynamoose');
 
 const messageDeliverySchema = new dynamoose.Schema({
-	id: {
-		type: String,
-		hashKey: true,
-	},
-	userId: {
-		type: String,
-	},
-	chatId: {
-		type: String,
-	},
-	platform: {
-		type: String,
-	},
-	message: {
-		type: String,
-	},
-	deliveryStatus: {
-		type: String,
-	},
-	messageId: {
-		type: String
-	}
+  id: {
+    type: String,
+    hashKey: true,
+  },
+  userId: {
+    type: String,
+  },
+  chatId: {
+    type: String,
+  },
+  platform: {
+    type: String,
+  },
+  message: {
+    type: String,
+  },
+  deliveryStatus: {
+    type: String,
+  },
+  messageId: {
+    type: String,
+  },
+  responseStatus: {
+    type: String,
+  },
 });
 const options = {
-	create: false, // Create table in DB, if it does not exist,
-	update: true, // Update remote indexes if they do not match local index structure
-}
+  create: false, // Create table in DB, if it does not exist,
+  update: true, // Update remote indexes if they do not match local index structure
+};
 
 exports.MessageDelivery = dynamoose.model('wogi-message-deliveries', messageDeliverySchema, options);
