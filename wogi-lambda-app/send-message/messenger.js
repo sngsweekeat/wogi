9beec2bdfb21d7ad913fb7ec4d8d92b970d00e85
Hoon Ding Yi <hoon_ding_yi@tech.gov.sg>
2019-01-24 12:02:17 +0800

    DY|JQ add eslint and remove unnecessary files

--- wogi-lambda-app/send-message/messenger.js
+++ wogi-lambda-app/send-message/messenger.js
@@ -1,69 +1,67 @@
-const axios = require("axios");
+const axios = require('axios');
+
+const FB_MESSENGER_URL = 'https://graph.facebook.com/v2.6/me/messages';
+
+const callMessengerSendAPI = async (chatId, message) => {
+  console.log('callMessengerSendAPI...');
+  // Construct the message body
+  const body = {
+    recipient: {
+      id: chatId,
+    },
+    message,
+  };
+  const params = { access_token: process.env.PAGE_ACCESS_TOKEN };
+  // Send the HTTP request to the Messenger Platform
+  const response = await axios.post(FB_MESSENGER_URL, body, { params });
+  return response;
+};
+
 
 exports.handler = async (chatId, message) => {
-	try {
-
-		let msg = {
-			"text": message
-		}
-		let msgWithButton = msg = {
-			"attachment": {
-				"type": "template",
-				"payload": {
-					"template_type": "button",
-					"text": message,
-					"buttons": [
-						{
-							"type": "postback",
-							"title": "Yes",
-							"payload": "{ messageDeliveryId, optionValue: \"Yes\" }"
-						},
-						{
-							"type": "postback",
-							"title": "No",
-							"payload": "{ messageDeliveryId, optionValue: \"Yes\" }"
-						},
-						{
-							"type": "postback",
-							"title": "Don't bother me",
-							"payload": "{ messageDeliveryId, optionValue: \"Yes\" }"
-						}
-					]
-				}
-			}
-		}
-		console.log("Sending message through messenger...");
-		const result = await callMessengerSendAPI(chatId, msgWithButton);
-		deliveryStatus = "SUCCESS";
-		console.log("Messenger result is: ", result);
-		return deliveryStatus;
-	}
-	catch (e) {
-		console.log('error is: ', e);
-		if (e.response.status >= 400 && e.response.status < 500) {
-			deliveryStatus = "FAIL";
-		}
-		else {
-			throw e;
-		}
-	}
-
-}
-
-const FB_MESSENGER_URL = "https://graph.facebook.com/v2.6/me/messages";
-
-const callMessengerSendAPI = async (sender_psid, message) => {
-	console.log("callMessengerSendAPI...");
-	// Construct the message body
-	const body = {
-		"recipient": {
-			"id": sender_psid
-		},
-		"message": message
-	}
-	const params = { "access_token": process.env.PAGE_ACCESS_TOKEN };
-	// Send the HTTP request to the Messenger Platform
-	const response = await axios.post(FB_MESSENGER_URL, body, { params });
-	return response;
-}
-
+  let deliveryStatus;
+  try {
+    // const msg = {
+    //   text: message,
+    // };
+    const msgWithButton = {
+      attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'button',
+          text: message,
+          buttons: [
+            {
+              type: 'postback',
+              title: 'Yes',
+              payload: '{ messageDeliveryId, optionValue: "Yes" }',
+            },
+            {
+              type: 'postback',
+              title: 'No',
+              payload: '{ messageDeliveryId, optionValue: "Yes" }',
+            },
+            {
+              type: 'postback',
+              title: "Don't bother me",
+              payload: '{ messageDeliveryId, optionValue: "Yes" }',
+            },
+          ],
+        },
+      },
+    };
+    console.log('Sending message through messenger...');
+    const result = await callMessengerSendAPI(chatId, msgWithButton);
+    deliveryStatus = 'SUCCESS';
+    console.log('Messenger result is: ', result);
+    return deliveryStatus;
+  } catch (e) {
+    console.log('error is: ', e);
+    if (e.response.status >= 400 && e.response.status < 500) {
+      deliveryStatus = 'FAIL';
+    } else {
+      throw e;
+    }
+    return deliveryStatus;
+  }
+};
--- wogi-lambda-app/send-message/telegram.js
+++ wogi-lambda-app/send-message/telegram.js
@@ -3,17 +3,14 @@ const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
 
 exports.handler = async (chatId, message) => {
-	try {
-		const result = await bot.sendMessage(chatId, message);
-		console.log("Telegram result is: ", result);
-		return deliveryStatus = "SUCCESS";
-	}
-	catch (e) {
-		if (e.response.statusCode >= 400 && e.response.statusCode < 500) {
-			deliveryStatus = "FAIL";
-		}
-		else {
-			throw e;
-		}
-	}
-}
+  try {
+    const result = await bot.sendMessage(chatId, message);
+    console.log('Telegram result is: ', result);
+    return 'SUCCESS';
+  } catch (e) {
+    if (e.response.statusCode >= 400 && e.response.statusCode < 500) {
+      return 'FAIL';
+    }
+    throw e;
+  }
+};
