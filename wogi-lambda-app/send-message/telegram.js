0c617ba3b0a564c596d969449a17482e4b12b3e5
Jun Qi <jqtan@thoughtworks.com>
2019-01-24 11:47:54 +0800

    Add missing async

--- wogi-lambda-app/send-message/telegram.js
+++ wogi-lambda-app/send-message/telegram.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
 
-exports.handler = (chatId, message) => {
+exports.handler = async (chatId, message) => {
 	try {
 		const result = await bot.sendMessage(chatId, message);
 		console.log("Telegram result is: ", result);
